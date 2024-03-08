42fec1d2fa533781ba2d292ede333af19709a5eb
supaaasuge <158092262+supaaasuge@users.noreply.github.com>
2024-03-08 17:51:42 -0500

    Update createToC.py

--- createToC.py
+++ createToC.py
@@ -35,14 +35,16 @@ def process_file(file_path):
                     heading = line.strip('###### ')
                     anchor = heading.replace(' ', '\\')
                     TableOfContents += f"          - [{heading}](#{anchor})\n"
-    except UnicodeDecodeError:
-        print(f"Skipping file {file_path} due to encoding issues")
+
         with open(file_path, 'r+', encoding='utf-8') as f:
             original_contents = f.read()
             f.seek(0)
             f.write(TableOfContents + "\n" + original_contents)
+        
+        print(f"Table of Contents appended successfully to {file_path}\r\nTotal: {counter}")
 
-    print(f"Table of Contents appended successfully to {file_path}")
+    except UnicodeDecodeError:
+        print(f"Skipping file {file_path} due to encoding issues")
 
 def process_directory(directory):
     for root, dirs, files in os.walk(directory):
@@ -51,34 +53,28 @@ def process_directory(directory):
                 file_path = os.path.join(root, file)
                 process_file(file_path)
 
-def main():
-    if args.dir:
-        directory = args.dir
-        if not os.path.isdir(directory):
-            print(f"{directory} is not a valid directory.")
-            sys.exit(1)
-        process_directory(directory)
-    else:
-        print("Please provide the directory path using the --dir argument.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Append Table of Contents to Markdown files.')
+    parser.add_argument('--dir', type=str, help='Directory containing Markdown files')
+    parser.add_argument('--file', type=str, help='File to process and append Table of Contents')
+    args = parser.parse_args()
+
+    if args.file and args.dir:
+        print("Please use either --file or --dir, but not both at the same time.")
         sys.exit(1)
-    
+
     if args.file:
         file = args.file
         if not os.path.isfile(file):
             print(f"{file} is not a valid file path.")
             sys.exit(1)
         process_file(file)
+    elif args.dir:
+        directory = args.dir
+        if not os.path.isdir(directory):
+            print(f"{directory} is not a valid directory.")
+            sys.exit(1)
+        process_directory(directory)
     else:
-        print("Please provide the correct file path using the --file argument.")
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Append Table of Contents to Markdown files.')
-    parser.add_argument('--dir', type=str, help='Directory containing Markdown files')
-    parser.add_argument('--file', type=str, help='File to process and append Table of Contents')
-    args = parser.parse_args()
-    main()
-
-    if args.file and args.dir is True:
-        print("Please use either --file or --dir, but not both at the same time.")
+        print("Please provide either the file path using --file or the directory path using --dir.")
         sys.exit(1)
